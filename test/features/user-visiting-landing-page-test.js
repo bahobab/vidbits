3fdc0d03ba36854b4f26806786ad3e93e393765e
mono motapa <bahobab@hotmail.com>
2018-01-20 18:55:50 -0600

    step 6: red test

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -12,4 +12,15 @@ describe('user visits landing page', () => {
             assert.equal(videoElements, landingPageElement);
         });
     });
+
+    describe('navigate to create page', () => {
+        it('click link to go to create video page', () => {
+            // set up & exercise
+            browser.url('/');
+            browser.click('a.create-videos')
+            var videosPage = 'Save a video';
+            // assert: the create page contains 'Save a video'
+            assert.include(browser.getText('body'), videosPage);
+        })
+    });
 });
